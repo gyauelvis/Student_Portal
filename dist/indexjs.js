0e17eea9ac768397f747f6e153dd4a8c19fe597a
g.elvis <gyauelvis@gmail.com>
2023-06-04 17:26:05 +0100

    Can't scroll when the nav menu pops up now

--- dist/indexjs.js
+++ dist/indexjs.js
@@ -2,21 +2,26 @@ let nav_menu_status = false;
 document.querySelector("#hamburger").addEventListener("click", (e) => {
   let nav_menu = document.querySelector("#nav_menu");
   e.preventDefault();
+  document.body.classList.add("fixed");
   if (!nav_menu_status) {
-    nav_menu.classList.replace("invisible", "visible");
-    nav_menu.classList.replace("w-[0]", "w-[85%]");
-    nav_menu_status = true;
     document
       .querySelectorAll(".inner_menu")
       .forEach((menu) => menu.classList.replace("invisible", "visible"));
-    
+    setTimeout(() => {
+      nav_menu.classList.replace("invisible", "visible");
+      nav_menu.classList.replace("w-[0]", "w-[85%]");
+      nav_menu_status = true;
+    }, 50);
   } else {
     e.preventDefault();
-    nav_menu.classList.replace("visible", "invisible");
-    nav_menu.classList.replace("w-[85%]", "w-[0]");
-    nav_menu_status = false;
+    document.body.classList.remove("fixed");
     document
       .querySelectorAll(".inner_menu")
       .forEach((menu) => menu.classList.replace("visible", "invisible"));
+    setTimeout(() => {
+      nav_menu.classList.replace("visible", "invisible");
+      nav_menu.classList.replace("w-[85%]", "w-[0]");
+      nav_menu_status = false;
+    }, 50);
   }
 });
